955adef958abe27e04a07e9b69f5a45710090bbd
RayashiAkira <95389276+RayashiAkira@users.noreply.github.com>
2024-10-22 14:07:45 -0300

    Add files via upload

---
+++ central_doacoes/central_doacoes/src/main/java/com/example/central_doacoes/CentralDoacoesApplication.java
@@ -0,0 +1,13 @@
+package com.example.central_doacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralDoacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralDoacoesApplication.class, args);
+	}
+
+}
